0093fad7c804a5a8dfba2fefd696d57f7b2f980f
haya02 <2595466596@qq.com>
2024-04-18 05:27:46 +0200

    first edition can run but with result false

--- trans/tools.py
+++ trans/tools.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.image import imread
 import math
+import select
+import sys
 
 #读取图片点击位置的像素坐标并通过转换矩阵转换为经纬度
 def select_points_and_transform(image_path, H,pix_coor):
@@ -39,11 +41,10 @@ def select_points_and_transform(image_path, H,pix_coor):
     print(test_coor_wld)
 
     while True:
-        choice = input("是否继续选择点？(yes/no): ")
-        if choice.lower() == "no":
-            break
         print("请点击图像选择一个点")
-        point = plt.ginput(1, show_clicks=True)
+        point = plt.ginput(1, timeout=0)
+        if not point:
+            break
         x, y = point[0]
 
         # 将像素点坐标转换为UTM坐标
@@ -52,43 +53,17 @@ def select_points_and_transform(image_path, H,pix_coor):
 
         lat, lon = transformer_t.transform(utm_coord[0], utm_coord[1])
         wld_coordinates_select.append((lon,lat))
-        
+    
         # 在图上标记点并显示坐标
         ax.plot(x, y, 'ro', markersize=5)  # 在图上标点
         ax.text(x, y, f'({lat:.6f}, {lon:.6f})', fontsize=8, color='black', ha='right')  # 在点旁边添加坐标文本
         print("坐标:",lat,lon)
         plt.draw()
+
+#参考一开始的取点函数
     plt.show()
 
 #转换经纬度坐标到utm
-def convert_to_utm(pixel_coordinates_w, from_crs="epsg:4326", to_crs="epsg:32649"):
-    """
-    将像素坐标转换为UTM坐标
-    
-    参数：
-    参数1：WGS84地理坐标系统 对应 4326 
-    参数2：坐标系WKID 东莞市 WGS_1984_UTM_Zone_49N 对应 32649    
-    pixel_coordinates_w: list，像素坐标组，每个元素是一个二元元组 (x, y)
-    from_crs: str，输入坐标系，默认为WGS84 (EPSG:4326)
-    to_crs: str，输出坐标系，默认为UTM Zone 49N (EPSG:32649)
-    
-    返回值：
-    pixel_coordinates_utm: list，UTM坐标组，每个元素是一个二元元组 (x, y)
-    """    
-    # 创建坐标转换器
-    transformer = Transformer.from_crs(from_crs, to_crs)
-    
-    # 将像素坐标转换为UTM坐标
-    pixel_coordinates_utm = []
-    for x, y in pixel_coordinates_w:
-        utm_x, utm_y = transformer.transform(x, y)  # 注意纬度在前，经度在后
-        utm_x = "{:.14f}".format(utm_x)
-        utm_y = "{:.14f}".format(utm_y)
-        pixel_coordinates_utm.append((utm_x, utm_y))
-        
-    return pixel_coordinates_utm
-
-
 def convert_to_utm(pixel_coordinates_w, from_crs="epsg:4326", to_crs="epsg:32649"):
     """
     将像素坐标转换为UTM坐标
@@ -127,16 +102,7 @@ def convert_to_utm(pixel_coordinates_w, from_crs="epsg:4326", to_crs="epsg:32649
 
 #转换utm坐标到经纬度
 def convert_to_latilon(pixel_coordinates, from_crs="epsg:32649", to_crs="epsg:4326"):
-    # 创建坐标转换器
-    transformer = Transformer.from_crs(from_crs, to_crs)
-    
-    # 将像素坐标转换为latilon坐标
-    pixel_coordinates_w = []
-    for x, y in pixel_coordinates:
-        w_x, w_y = transformer.transform(x, y)  # 注意纬度在前，经度在后
-        w_x = "{:.14f}".format(w_x)
-        w_y = "{:.14f}".format(w_y)
-        pixel_coordinates_w.append((w_x, w_y))        
+    pixel_coordinates_w = convert_to_utm(pixel_coordinates,to_crs,from_crs)      
     return pixel_coordinates_w
 
 #连成闭合获取角点坐标但最后一个点会删除
